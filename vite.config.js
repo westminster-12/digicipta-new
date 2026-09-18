386467415addee0ed99547d514a4157ab87fabea
Jeanne Ambana <jeanneambana@gmail.com>
2026-09-18 15:29:18 +0800

    fix routing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
   build: {
     // Peringatkan jika chunk > 500 KB
